bf2f49d123e0bf87f1ad620081aba099977bcfbc
Thomas W <netzberufler@googlemail.com>
2015-08-26 16:40:30 +0200

    reviewed and cleaned up translation strings for better localization

--- functions.php
+++ functions.php
@@ -256,7 +256,7 @@ function courage_list_comments($comment, $args, $depth) {
 
 				<div class="comment-author vcard">
 					<?php echo get_avatar( $comment, 56 ); ?>
-					<?php printf(__('<span class="fn">%s</span>', 'courage'), get_comment_author_link()) ?>
+					<?php printf( '<span class="fn">%s</span>', get_comment_author_link() ); ?>
 				</div>
 
 		<?php if ($comment->comment_approved == '0') : ?>
