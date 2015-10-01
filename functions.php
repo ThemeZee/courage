06bb407c94b31b041193f72d11f7ee63255ac9b7
Thomas W <netzberufler@googlemail.com>
2015-10-01 20:10:13 +0200

    added support for widget bundle

--- functions.php
+++ functions.php
@@ -104,9 +104,6 @@ function courage_setup() {
 		'width'	=> 1340,
 		'height' => 200,
 		'flex-height' => true));
-	
-	// Add Theme Support for Courage Pro Plugin
-	add_theme_support( 'courage-pro' );
 
 	// Register Navigation Menus
 	register_nav_menu( 'primary', __('Main Navigation', 'courage') );
@@ -184,6 +181,9 @@ require get_template_directory() . '/inc/extras.php';
 
 // include Template Functions
 require( get_template_directory() . '/inc/template-tags.php' );
+
+// Include support functions for Theme Addons
+require get_template_directory() . '/inc/addons.php';
 
 // include Widget Files
 require( get_template_directory() . '/inc/widgets/widget-category-posts-boxed.php' );
