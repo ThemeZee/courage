c5e5a8a0d2c4747f798cf52c7375103ef3badd18
Thomas W <netzberufler@googlemail.com>
2016-05-10 12:15:47 +0200

    added support for selective refresh in customizer

--- functions.php
+++ functions.php
@@ -129,6 +129,9 @@ function courage_setup() {
 	
 	// Register Social Icons Menu
 	register_nav_menu( 'social', esc_html__( 'Social Icons', 'courage' ) );
+	
+	// Add Theme Support for Selective Refresh in Customizer
+	add_theme_support( 'customize-selective-refresh-widgets' );
 
 }
 
